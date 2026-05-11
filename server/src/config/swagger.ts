f79447253c627e043310edecc676d05311f5e56b
AkshayPP-3 <24061100617@reva.edu.in>
2026-05-11 23:13:21 +0530

    made swagger docs ui

---
+++ server/src/config/swagger.ts
@@ -0,0 +1,20 @@
+import swaggerJsdoc from "swagger-jsdoc";
+
+const option= {
+    definition: {
+        opeanapi: "3.0.0",
+        info: {
+            title: "fluxChat API",
+            version:"1.0.0",
+            description:"API documentation for backend",
+        },
+        servers: [
+            {
+                url: "http://localhost:3000",
+            },
+        ],
+    },
+    apis: ["./src/routes/*.ts"],
+};
+
+export const specs = swaggerJsdoc(option);
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,9 @@ import dotenv from "dotenv";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import swaggerUi from "swagger-ui-express";
+import {specs} from "./config/swagger.js"
+import { spec } from "node:test/reporters";
 
 dotenv.config();
 
@@ -13,6 +16,7 @@ const PORT = process.env.PORT
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
+app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(specs));
 
 app.get("/test",(_,res)=>{
     res.send("Test message")
