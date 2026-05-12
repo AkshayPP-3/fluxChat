329b0a0d7a1a4bc599ee3e5c5f5904efc255af84
AkshayPP-3 <24061100617@reva.edu.in>
2026-05-13 01:14:50 +0530

    wire up express middleware and auth routes in server entry

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import swaggerUi from "swagger-ui-express";
+import authRoutes from "./routes/authRoutes.js";
 
 import {specs} from "./config/swagger.js"
 
@@ -18,6 +19,8 @@ app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(specs));
+app.use("/api/auth",authRoutes);
+
 
 app.get("/test",(_,res)=>{
     res.send("Test message")
