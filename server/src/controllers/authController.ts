93afeead1cf630c5178667296f0bfa8c97b60f4b
AkshayPP-3 <24061100617@reva.edu.in>
2026-05-13 00:11:27 +0530

    added user login controller

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -2,6 +2,8 @@ import type { Request, Response } from "express";
 import bcrypt from "bcryptjs";
 import { prisma } from "../lib/prisma.js"
 
+import jwt from "jsonwebtoken"; 
+
 export const registerUser = async(req:Request,res:Response)=>{
     try{
         const {firstname, lastname, username,password,confirmPassword}=req.body;
@@ -39,5 +41,49 @@ export const registerUser = async(req:Request,res:Response)=>{
     }catch(error){
         console.log(error);
         return res.status(500).json({message:"Internal server error"})
+    }
+}
+
+export const loginUser = async(req:Request,res:Response)=>{
+    try{
+        const{username,password} = req.body;
+        if(!username || !password){
+            res.status(400).json({message:"All fields are required"})
+        }
+        const user = await prisma.user.findUnique({
+            where: {username}
+        })
+        if(!user){
+            return res.status(400).json({message: "Invalid credentials"})
+        }
+
+        //compare password
+        const isMatch = await bcrypt.compare(password,user.password);
+
+        if(!isMatch){
+            return res.status(400).json({message: "Invalid credentials"})
+        }
+
+        //generate jwt
+
+        const token = jwt.sign(
+            { userId: user.id },
+            process.env.JWT_SECRET as string,
+            { expiresIn: "7d" }
+        );
+
+        return res.status(200).json({
+            message: "Login successful",
+            token,
+            user: {
+                id: user.id,
+                firstname: user.firstname,
+                lastname: user.lastname,
+                username: user.username,
+            },
+        });
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({message: "Internal server error"})
     }
 }
