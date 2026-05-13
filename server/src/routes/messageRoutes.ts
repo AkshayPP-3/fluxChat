680a25cd513cb723c3622a74bf33380d561565ef
AkshayPP-3 <24061100617@reva.edu.in>
2026-05-13 17:37:35 +0530

    added message routes for chat system

---
+++ server/src/routes/messageRoutes.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import { sendMessage,getMessages } from "../controllers/messageController";
+import { protect } from "../middleware/authMiddleware";
+
+const router = express.Router();
+ 
+router.post("/",protect,sendMessage);
+
+router.get("/:conversationId",protect,getMessages);
+
+export default router;
