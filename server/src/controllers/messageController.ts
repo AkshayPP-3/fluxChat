e1c97237ebdd4cbdb3504971d43e10bd08d40d21
AkshayPP-3 <24061100617@reva.edu.in>
2026-05-13 17:33:57 +0530

    added message controller for sending and fetching chat message

--- server/src/controllers/messageController.ts
+++ server/src/controllers/messageController.ts
@@ -31,3 +31,35 @@ export const sendMessage = async (req:Request,res:Response)=>{
         return res.status(500).json({message: "internal server error"})
     }
 }
+
+export const getMessages = async(req:Request,res:Response)=>{
+    try{
+        const conversationId=req.params.conversationId as string;
+        if(!conversationId){
+            return res.status(400).json({message: "all fields are required"})
+        }
+
+        const messages = await prisma.message.findMany({
+            where: {
+                conversationId
+            },
+            orderBy: {
+                createdAt: "asc",
+            },
+            include: {
+                sender: {
+                    select: {
+                        id: true,
+                        firstname: true,
+                        lastname: true,
+                        username: true
+                    }
+                }
+            }
+        })
+        return res.status(200).json(messages);
+    }catch(error){
+        console.log(error)
+        return res.status(500).json({message: "internal server error"})
+    }
+}
