9927c71a2dd223087a3db3ec60816a89617dd140
AkshayPP-3 <24061100617@reva.edu.in>
2026-05-14 00:16:12 +0530

    setup Socket.IO server for realtime chat communication

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,12 +3,16 @@ import dotenv from "dotenv";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-import swaggerUi from "swagger-ui-express";
+
 import authRoutes from "./routes/authRoutes.js";
 import conversationRoutes from "./routes/conversationRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js"
 
-import {specs} from "./config/swagger.js"
+import {specs} from "./config/swagger.js";
+import swaggerUi from "swagger-ui-express";
+
+import http from "http";
+import {initSocket} from "./sockets/socket.js";
 
 dotenv.config();
 
@@ -16,11 +20,15 @@ const app = express();
 
 const PORT = process.env.PORT || 5000
 
+const server = http.createServer(app);
+
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
+
 app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(specs));
+
 app.use("/api/auth",authRoutes);
 app.use("/api/conversations",conversationRoutes);
 app.use("/api/messages",messageRoutes);
@@ -28,6 +36,9 @@ app.use("/api/messages",messageRoutes);
 app.get("/test",(_,res)=>{
     res.send("Test message")
 })
-app.listen(PORT,()=>{
+
+initSocket(server);
+
+server.listen(PORT,()=>{
     console.log(`server running on port ${PORT}`)
 })
---
+++ server/src/sockets/socket.ts
@@ -0,0 +1,31 @@
+import {Server} from "socket.io";
+export const initSocket = (server: any)=>{
+    const io = new Server(server,{
+        cors:{
+            origin: "*",
+            methods: ["GET","POST"]
+        }
+    })
+    io.on("connection",(socket)=>{
+        console.log("user connected:",socket.id);
+
+        socket.on("join_conversation",(conversationId)=>{
+            socket.join(conversationId);
+            console.log(`Joined room: ${conversationId}`);
+        })
+
+        socket.on("send_message",(data)=>{
+            io.to(data.conversationId).emit("receive_message",{
+                conversationId: data.conversationId,
+                message:data.message,
+                senderId: data.senderId,
+                createdAt: new Date(),
+            })
+        })
+
+        socket.on("discount", ()=>{
+            console.log("User disconnected: ",socket.id);
+        })
+    })
+    return io;
+}
