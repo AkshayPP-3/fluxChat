9f8c4c156c96af20bc6495d1c7bd55c743420eb2
AkshayPP-3 <24061100617@reva.edu.in>
2026-05-14 18:29:06 +0530

    add swagger documentation for auth register user route

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -4,7 +4,35 @@ import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/register",registerUser);
+/**
+ * @swagger
+ * /api/auth/register:
+ *   post:
+ *     summary: Register a new user
+ *     tags:
+ *       - Auth
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               firstname:
+ *                 type: string
+ *               lastname:
+ *                 type: string
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               confirmPassword:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ */
+router.post("/register", registerUser);
 
 router.post("/login",loginUser);
 
