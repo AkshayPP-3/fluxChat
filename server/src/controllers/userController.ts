e99026cffb3f12aa0bf120b841d49a9ff88f221f
AkshayPP-3 <24061100617@reva.edu.in>
2026-05-14 17:33:34 +0530

    added get user by id controller

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,7 +1,8 @@
 import type { Request,Response } from "express";
 import {prisma} from "../lib/prisma.js";
+import { use } from "react";
 
-export const getAllUsers = async (req:Request,res: Response)=>{
+export const getAllUsers = async (_:Request,res: Response)=>{
     try{
         const users = await prisma.user.findMany({
             select: {
@@ -16,5 +17,30 @@ export const getAllUsers = async (req:Request,res: Response)=>{
     }catch(error){
         console.log(error);
         return res.status(500).json({message: "internal server error"});
+    }
+}
+
+export const getUserById = async(req:Request,res:Response)=>{
+    try{
+        const id = req.params.id as string;
+        const user = await prisma.user.findUnique({
+            where: {
+                id
+            },
+            select: {
+                id: true,
+                firstname: true,
+                lastname: true,
+                username: true,
+                createdAt: true
+            }
+        })
+        if(!user){
+            return res.status(404).json({message: "user not found"})
+        }
+        return res.status(200).json(user);
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({message: "internal server error"})
     }
 }
