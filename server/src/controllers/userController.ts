7d796c77ec0bb665b16e6f3d899ca27669d9860e
AkshayPP-3 <24061100617@reva.edu.in>
2026-05-14 17:40:45 +0530

    added search users controller

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,6 +1,5 @@
 import type { Request,Response } from "express";
 import {prisma} from "../lib/prisma.js";
-import { use } from "react";
 
 export const getAllUsers = async (_:Request,res: Response)=>{
     try{
@@ -42,5 +41,49 @@ export const getUserById = async(req:Request,res:Response)=>{
     }catch(error){
         console.log(error);
         return res.status(500).json({message: "internal server error"})
+    }
+}
+
+export const searchUsers = async (req: Request,res:Response)=>{
+    try{
+        const query = req.query.q as string;
+
+        if(!query){
+            return res.status(400).json({message: " search query is required"})
+        }
+        const users = await prisma.user.findMany({
+            where: {
+                OR: [
+                    {
+                        firstname: {
+                            contains: query,
+                            mode: "insensitive"
+                        }
+                    },
+                    {
+                        lastname: {
+                            contains: query,
+                            mode: "insensitive"
+                        }
+                    },
+                    {
+                        username: {
+                            contains: query,
+                            mode: "insensitive"
+                        }
+                    }
+                ]
+            },
+            select: {
+                id: true,
+                firstname: true,
+                lastname: true,
+                username: true,
+            }
+        })
+        return res.status(200).json(users);
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({message: "internal server error"});
     }
 }
