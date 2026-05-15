dec4d4b906a2b4a21f9f542a62cf450b2632ff85
AkshayPP-3 <24061100617@reva.edu.in>
2026-05-15 14:26:15 +0530

    add swagger for get user by id

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -32,6 +32,36 @@ const router = express.Router();
  */
 router.get("/",protect,getAllUsers);
 
+/**
+ * @swagger
+ * /api/users/{id}:
+ *   get:
+ *     summary: Get user by ID
+ *     tags:
+ *       - Users
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The user ID
+ *     responses:
+ *       200:
+ *         description: User found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: User not found
+ *       500:
+ *         description: Internal server error
+ */
 router.get("/:id",protect,getUserById);
 
 router.get("/search",protect,searchUsers);
