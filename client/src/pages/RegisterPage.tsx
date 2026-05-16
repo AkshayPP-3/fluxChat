d5cce00d95e0d8075705807ee0ac0e890c2b0a8a
AkshayPP-3 <24061100617@reva.edu.in>
2026-05-16 18:30:59 +0530

    made a simple register page for checking

---
+++ client/src/pages/RegisterPage.tsx
@@ -0,0 +1,9 @@
+function RegisterPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-3xl font-bold">Register Page</h1>
+    </div>
+  );
+}
+
+export default RegisterPage;
